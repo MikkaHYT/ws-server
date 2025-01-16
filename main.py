98f679ffa3717d221fd6c3d2d455d7f08f0cc244
Mikka <44730779+MikkaHYT@users.noreply.github.com>
2025-01-16 16:17:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ async def broadcast(data):
         message = json.dumps(data)
         await asyncio.wait([client.send(message) for client in connected_clients])
 
-start_server = websockets.serve(handler, "0.0.0.0", 8000)
+start_server = websockets.serve(handler, "localhost", 8000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
